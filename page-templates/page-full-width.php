2ecbc966e2db901bf3f16c4ef36506e49e634c8e
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-05-10 16:59:41 -0300

    imagen destacada en pagina

--- page-templates/page-full-width.php
+++ page-templates/page-full-width.php
@@ -4,9 +4,9 @@ Template Name: Full Width
 */
 get_header(); ?>
 
-<?php //get_template_part( 'template-parts/featured-image' ); ?>
+
 <div class="main-wrap">
-	
+		
 		<main class="main-content-full-width">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
